3b081f1cb694c4146f5ee782c7b756096a568f99
Dan <36828197+dangraham99@users.noreply.github.com>
2022-08-22 17:04:12 +0100

    navigation to details screen with params

--- src/pages/Groups/GroupsScreen.jsx
+++ src/pages/Groups/GroupsScreen.jsx
@@ -5,18 +5,23 @@ import { useTheme } from '@react-navigation/native';
 import GroupCard from '../../components/GroupCard';
 import currency from 'currency.js';
 
-export default function GroupsScreen() {
+export default function GroupsScreen({ navigation }) {
 
     const { colors } = useTheme()
+
+    const testData = ["Friday night", 3, 400]
+    const testData2 = ["John's Birthday", 8, -732]
 
     return (
         <SafeAreaView style={styles.safearea}>
             <ScrollView style={styles.container}>
 
-                <GroupCard title={"Friday night"} participants={3} balance={400} />
+
+                <GroupCard onPress={() => { navigation.navigate('Details', { data: testData }) }} title={testData[0]} participants={testData[1]} balance={testData[2]} />
 
 
-                <GroupCard title={"John's birthday"} participants={8} balance={-732} />
+
+                <GroupCard onPress={() => { navigation.navigate('Details', { data: testData2 }) }} title={testData2[0]} participants={testData2[1]} balance={testData2[2]} />
 
 
             </ScrollView>
