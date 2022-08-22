e91a5d9910d89122ae19a172fca0fecf0b519cf1
Dan <36828197+dangraham99@users.noreply.github.com>
2022-08-22 17:03:42 +0100

    text cut off max lines 1

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { styles } from '../assets/styles'
 import { Ionicons } from '@expo/vector-icons';
@@ -6,34 +6,30 @@ import currency from 'currency.js';
 
 export default function GroupCard(props) {
     return (
-        <View style={styles.groupCard}>
-            <View style={{ flex: 3 }}>
-                <Text style={styles.cardSubtitle}>{`${props.participants} participants`}</Text>
-                <Text style={styles.cardTitle}>{props.title}</Text>
-
+        <Pressable
+            onPress={() => {
+                props.onPress()
+            }
+            }>
+            <View style={styles.groupCard}>
+                <View style={{ flex: 4 }}>
+                    <Text style={styles.cardSubtitle}>{`${props.participants} participants`}</Text>
+                    <Text numberOfLines={1} style={styles.cardTitle}>{props.title}</Text>
+                </View>
+                <View style={{ flex: 2, alignItems: 'flex-end', justifyContent: 'flex-center' }}>
+                    <Text style={styles.cardSubtitle}>{props.balance < 0 ? "You owe" : "You're owed"}</Text>
+                    <Text style={[styles.cardTitle, {
+                        color: props.balance < 0 ? 'red' : 'black'
+                    }]}>{currency(props.balance, { fromCents: true, symbol: "£" }).format()}</Text>
+                </View>
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'flex-center',
+                    alignItems: 'flex-end'
+                }}>
+                    <Ionicons name="ios-chevron-forward" size={32} color={'grey'} />
+                </View>
             </View>
-
-
-            <View style={{ flex: 3, alignItems: 'flex-end', justifyContent: 'flex-center' }}>
-
-                <Text style={styles.cardSubtitle}>{props.balance < 0 ? "You owe" : "You're owed"}</Text>
-                <Text style={[styles.cardTitle, {
-                    color: props.balance < 0 ? 'red' : 'black'
-                }]}>{currency(props.balance, { fromCents: true, symbol: "£" }).format()}</Text>
-
-            </View>
-
-            <View style={{
-                flex: 1,
-                justifyContent: 'flex-center',
-                alignItems: 'flex-end'
-            }}>
-
-                <Ionicons name="ios-chevron-forward" size={32} color={'grey'} />
-
-            </View>
-
-
-        </View>
+        </Pressable>
     )
 }
