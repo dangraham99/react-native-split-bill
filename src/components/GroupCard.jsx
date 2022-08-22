8bf9d3ec11a378c0822461cbf890bd7aaddebda6
Dan <36828197+dangraham99@users.noreply.github.com>
2022-08-22 23:28:50 +0100

    profile picture heads

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -1,10 +1,9 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, Image } from 'react-native'
 import React from 'react'
 import { styles } from '../assets/styles'
 import { Ionicons } from '@expo/vector-icons';
 import currency from 'currency.js';
 import { useTheme } from '@react-navigation/native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function GroupCard(props) {
 
@@ -20,8 +19,16 @@ export default function GroupCard(props) {
                 <View style={{ flex: 4 }}>
                     <Text style={styles.cardSubtitle}>{`${props.participants} participants`}</Text>
                     <Text numberOfLines={1} style={[styles.cardTitle, { color: colors.text }]}>{props.title}</Text>
+                    <View style={[styles.groupCardPictures, { marginTop: 10 }]}>
+
+                        <Image style={[styles.profileImg, { position: 'absolute' }]} source={require(`../assets/placeholders/portrait-1.jpg`)} />
+                        <Image style={[styles.profileImg, { position: 'absolute', left: 25 }]} source={require(`../assets/placeholders/portrait-2.jpg`)} />
+                        <Image style={[styles.profileImg, { position: 'absolute', left: 50 }]} source={require(`../assets/placeholders/portrait-3.jpg`)} />
+                        <Image style={[styles.profileImg, { position: 'absolute', left: 75 }]} source={require(`../assets/placeholders/portrait-4.jpg`)} />
+                        {props.participants > 4 && <View style={[styles.profileImg, { position: 'absolute', left: 100, backgroundColor: colors.primary, justifyContent: 'center' }]}><Text style={{ alignSelf: 'center', fontWeight: 'bold', fontSize: 18, letterSpacing: 2, color: 'white' }}>+{props.participants - 4}</Text></View>}
+                    </View>
                 </View>
-                <View style={{ flex: 2, alignItems: 'flex-end', justifyContent: 'flex-center' }}>
+                <View style={{ flex: 2, alignItems: 'flex-end' }}>
                     <Text style={styles.cardSubtitle}>{props.balance < 0 ? "You owe" : "You're owed"}</Text>
                     <Text style={[styles.cardTitle, {
                         color: props.balance < 0 ? 'red' : colors.text
@@ -29,7 +36,7 @@ export default function GroupCard(props) {
                 </View>
                 <View style={{
                     flex: 1,
-                    justifyContent: 'flex-center',
+                    justifyContent: 'center',
                     alignItems: 'flex-end'
                 }}>
                     <Ionicons name="ios-chevron-forward" size={32} color={'grey'} />
