2e7555bb3c9738dd91d7741709044f2b0b4e45a0
Dan <36828197+dangraham99@users.noreply.github.com>
2022-08-23 20:04:42 +0100

    profile image border

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -16,7 +16,7 @@ export default function TransactionCard() {
     return (
         <View style={[styles.transactionCard, { backgroundColor: colors.card }]}>
             <View>
-                <Image style={styles.profileImg} source={require(`../assets/placeholders/portrait-7.jpg`)} />
+                <Image style={[styles.profileImg, { borderColor: colors.card }]} source={require(`../assets/placeholders/portrait-7.jpg`)} />
             </View>
             <View style={{ flex: 2, paddingLeft: 10 }}>
 
