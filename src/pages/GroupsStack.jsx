cb9ca47f586c53c817d7e0383c00ef9b9e6207a8
Dan <36828197+dangraham99@users.noreply.github.com>
2022-08-23 01:07:59 +0100

    navigate to/from create group modal screen

--- src/pages/GroupsStack.jsx
+++ src/pages/GroupsStack.jsx
@@ -1,21 +1,43 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useTheme } from '@react-navigation/native';
 import GroupsScreen from './Groups/GroupsScreen'
 import HomeScreen from './Home/HomeScreen';
 import { stackOptions } from '../assets/styles'
 import GroupDetailsScreen from './Groups/GroupDetailsScreen';
+import CreateGroupScreen from './Groups/CreateGroupScreen';
 
-export default function GroupsStack() {
+export default function GroupsStack({ navigation }) {
+
+
 
     const Stack = createNativeStackNavigator();
+
+    const { colors } = useTheme()
 
     return (
         <Stack.Navigator>
             <Stack.Screen name="Groups" options={{ headerLargeTitle: true }} component={GroupsScreen} />
-            <Stack.Screen name="Details" options={{ title: "Details", headerLargeTitle: true }} component={GroupDetailsScreen} />
-
-            <Stack.Screen name="New Group" options={{ headerTitle: "New Group", presentation: 'modal' }} component={HomeScreen} />
+            <Stack.Screen name="Details" options={{ headerTitle: "Details", headerLargeTitle: true }} component={GroupDetailsScreen} />
+            <Stack.Screen name="NewGroup" options={{
+                headerTitle: "New Group",
+                presentation: "modal",
+                headerLeft: () => (
+                    <Button
+                        onPress={() => navigation.navigate("Groups")}
+                        title="Cancel"
+                        color={'red'}
+                    />
+                ),
+                headerRight: () => (
+                    <Button
+                        onPress={() => alert('This is a button!')}
+                        title="Done"
+                        color={colors.primary}
+                    />
+                ),
+            }} component={CreateGroupScreen} />
 
 
         </Stack.Navigator>
