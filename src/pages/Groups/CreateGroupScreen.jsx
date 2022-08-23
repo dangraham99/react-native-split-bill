d226a4d73dc69cab8166b7a86c4ffd224258d939
Dan <36828197+dangraham99@users.noreply.github.com>
2022-08-23 16:11:47 +0100

    add placeholder user card

--- src/pages/Groups/CreateGroupScreen.jsx
+++ src/pages/Groups/CreateGroupScreen.jsx
@@ -1,8 +1,9 @@
-import { View, Text, TextInput, SafeAreaView } from 'react-native'
+import { View, Text, TextInput, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import { useTheme } from '@react-navigation/native'
 import React, { useState, useEffect } from 'react'
 import { styles } from '../../assets/styles'
 import { Picker } from '@react-native-picker/picker';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function CreateGroupScreen({ navigation }) {
 
@@ -18,15 +19,33 @@ export default function CreateGroupScreen({ navigation }) {
 
     return (
         <SafeAreaView style={styles.safearea}>
-            <View style={[styles.container, { marginTop: 20 }]}>
-                <View style={{ marginBottom: 15 }}>
+            <View style={{ marginTop: 20 }}>
+                <View style={[styles.container, { marginBottom: 15 }]}>
                     <Text style={styles.cardSubtitle}>Group Info</Text>
-                    <TextInput onChangeText={(value) => { setGroupName(value) }} placeholder='Group name' style={[styles.textInput, { color: colors.text, backgroundColor: colors.card }]} />
+                    <TextInput placeholderTextColor={'grey'} onChangeText={(value) => { setGroupName(value) }} placeholder='Group name' style={[styles.textInput, { color: colors.text, backgroundColor: colors.card }]} />
                 </View>
-                <Text style={styles.cardSubtitle}>Participants</Text>
-                <Text style={{ color: colors.text, marginTop: 5, fontSize: 13 }}>Invite people to join the group now. You can add more later.</Text>
+                <View style={[styles.container, { marginBottom: 15 }]}>
+                    <Text style={styles.cardSubtitle}>Participants</Text>
+                    <Text style={{ color: colors.text, marginTop: 5, fontSize: 13 }}>Invite people to join the group now. You can add more later.</Text>
+                </View>
+                <View style={[styles.transactionCard, { backgroundColor: colors.card }]}>
+                    <View>
+                        <Image style={[styles.profileImg, { borderColor: colors.card }]} source={require(`../../assets/placeholders/portrait-6.jpg`)} />
+                    </View>
+                    <View style={{ flex: 2, paddingLeft: 10, justifyContent: 'center' }}>
+
+                        <Text style={[styles.cardTitle, { color: colors.text }]}>Daniel Graham</Text>
+                        <Text style={[styles.cardSubtitle]}>Group Owner</Text>
+
+                    </View>
+                    <View style={{ alignItems: 'flex-end', justifyContent: 'center', flex: 1 }}>
+                        <TouchableOpacity>
+                            <Ionicons name="ios-information-circle-outline" size={28} color={colors.primary} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
             </View>
-        </SafeAreaView>
+        </SafeAreaView >
     )
 }
