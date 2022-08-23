04b948b2815406a85fe0ecc600ae564514de0be1
Dan <36828197+dangraham99@users.noreply.github.com>
2022-08-23 20:05:34 +0100

    tinted buttons and refactored navigation logic

--- src/pages/Groups/GroupsScreen.jsx
+++ src/pages/Groups/GroupsScreen.jsx
@@ -5,31 +5,28 @@ import { useTheme } from '@react-navigation/native';
 import GroupCard from '../../components/GroupCard';
 import currency from 'currency.js';
 import { Ionicons } from '@expo/vector-icons';
+import TintedButton from '../../components/TintedButton';
 
-export default function GroupsScreen({ navigation }) {
+export default function GroupsScreen(props) {
 
     const { colors } = useTheme()
 
     const testData = ["Friday night", 3, 400]
     const testData2 = ["John's Birthday", 8, -732]
-    const testData3 = ["Christmas Party", 20, 2032]
+    const testData3 = ["Christmas Party", 20, 5032]
 
     return (
         <SafeAreaView style={styles.safearea}>
             <ScrollView>
 
-                <TouchableOpacity onPress={() => { navigation.navigate('NewGroup') }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'center', marginVertical: 25 }}>
-                        <View style={[styles.tintedButton, { backgroundColor: colors.primaryTransparent }]}>
-                            <Ionicons name="add" size={32} color={colors.secondary} />
-                            <Text style={[styles.tintedButtonText, { color: colors.secondary }]}>New Group</Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
+                <TintedButton onPress={() => { props.navigation.navigate('New Group') }}>
+                    <Ionicons name="add" size={32} color={colors.secondary} />
+                    <Text style={[styles.tintedButtonText, { color: colors.secondary }]}>New Group</Text>
+                </TintedButton>
 
-                <GroupCard onPress={() => { navigation.navigate('Details', { data: testData }) }} title={testData[0]} participants={testData[1]} balance={testData[2]} />
-                <GroupCard onPress={() => { navigation.navigate('Details', { data: testData2 }) }} title={testData2[0]} participants={testData2[1]} balance={testData2[2]} />
-                <GroupCard onPress={() => { navigation.navigate('Details', { data: testData3 }) }} title={testData3[0]} participants={testData3[1]} balance={testData3[2]} />
+                <GroupCard onPress={() => { props.navigation.navigate('Details', { data: testData }) }} title={testData[0]} participants={testData[1]} balance={testData[2]} />
+                <GroupCard onPress={() => { props.navigation.navigate('Details', { data: testData2 }) }} title={testData2[0]} participants={testData2[1]} balance={testData2[2]} />
+                <GroupCard onPress={() => { props.navigation.navigate('Details', { data: testData3 }) }} title={testData3[0]} participants={testData3[1]} balance={testData3[2]} />
 
             </ScrollView>
         </SafeAreaView >
