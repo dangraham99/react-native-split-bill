742ad20e5278d8eed0732a0bb518cdd5fb712557
Dan <36828197+dangraham99@users.noreply.github.com>
2024-04-14 22:42:30 +0100

    temp layout

--- src/pages/Profile/ProfileScreen.jsx
+++ src/pages/Profile/ProfileScreen.jsx
@@ -1,15 +1,43 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import { styles } from '../../assets/styles'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Image } from 'react-native'
+import { fetchData } from '../../utils/fetchData'
 
 export default function ProfileScreen() {
 
+    const [profile, setProfile] = useState([])
+
+    const fetchProfileData = async () => {
+        try {
+            const data = await fetchData('http://localhost:3000/users/5');
+            setProfile(data); // Update groups state with fetched data
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchProfileData(); // Fetch groups data on component mount
+    }, []); // Run effect only once on component mount
 
 
     return (
         <SafeAreaView style={styles.safearea}>
             <ScrollView style={styles.container}>
-                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi labore cupiditate explicabo ipsam fugit rem inventore dolorem dolorum error praesentium blanditiis in saepe et itaque eligendi, ut eius perspiciatis ullam. Soluta facilis perspiciatis minus eligendi sequi corporis magni! Sapiente facilis tenetur eos eligendi perspiciatis corporis culpa voluptas vel numquam aliquam, ab error in mollitia perferendis, itaque repudiandae, molestiae corrupti. Natus et exercitationem voluptatum nisi id blanditiis quibusdam sit, laudantium iure ipsum asperiores in nulla recusandae consequuntur? Quasi, mollitia hic repellat nisi harum fuga quo et neque minima! Asperiores enim minima debitis veritatis? Ullam sint nesciunt minima, nulla rem laborum aspernatur.</Text>
+                <View style={[styles.container, styles.centerContainer]}>
+                    <Image width={200} height={200}
+                        style={{
+                            marginBottom: 20,
+                            resizeMode: 'cover',
+                            borderRadius: 360,
+                            borderWidth: 2
+                        }}
+                        source={{uri: profile.avatar}}
+                    />
+                </View>
+                <Text style={styles.pageTitle}>{profile.name}</Text>
+                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum non et ut dolor porro id quasi necessitatibus. Aspernatur asperiores quis doloribus, sequi dolorum quibusdam aliquid molestias facere exercitationem. Ea repellat nesciunt quia provident magni cumque quis dicta consequuntur rem saepe, vel ratione neque earum asperiores perspiciatis dolorem, est quaerat culpa?</Text>
             </ScrollView>
         </SafeAreaView>
     )
