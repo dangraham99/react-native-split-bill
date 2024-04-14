1e9d093afc779decc0e03dbb30d24bec62a67216
Dan <36828197+dangraham99@users.noreply.github.com>
2024-04-14 17:11:27 +0100

    adding request group data from local server

--- src/pages/Groups/GroupsScreen.jsx
+++ src/pages/Groups/GroupsScreen.jsx
@@ -1,29 +1,27 @@
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
-import { styles } from '../../assets/styles'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import { styles } from '../../assets/styles';
 import GroupCard from '../../components/GroupCard';
-import currency from 'currency.js';
-import { Ionicons } from '@expo/vector-icons';
-import TintedButton from '../../components/TintedButton';
+import { useData } from '../../utils/useData';
 
 export default function GroupsScreen(props) {
+    const { colors } = useTheme();
+    const groupsData = useData('http://localhost:3000/groups');
+    const [groups, setGroups] = useState([]);
 
-    const { colors } = useTheme()
-
-    const testData = ["Friday night", 3, 400]
-    const testData2 = ["John's Birthday", 8, -732]
-    const testData3 = ["Christmas Party", 20, 5032]
+    useEffect(() => {
+        // Update groups state with fetched data
+        setGroups(groupsData || []);
+    }, [groupsData]); // Run effect whenever groupsData changes
 
     return (
         <SafeAreaView style={styles.safearea}>
             <ScrollView>
-
-                <GroupCard onPress={() => { props.navigation.navigate('Details', { data: testData }) }} title={testData[0]} participants={testData[1]} balance={testData[2]} />
-                <GroupCard onPress={() => { props.navigation.navigate('Details', { data: testData2 }) }} title={testData2[0]} participants={testData2[1]} balance={testData2[2]} />
-                <GroupCard onPress={() => { props.navigation.navigate('Details', { data: testData3 }) }} title={testData3[0]} participants={testData3[1]} balance={testData3[2]} />
-
+                {groups.map((group, i) => (
+                    <GroupCard key={i} title={group.title} participants={group.users.length} balance={group.balancesInCents} />
+                ))}
             </ScrollView>
-        </SafeAreaView >
-    )
+        </SafeAreaView>
+    );
 }
---
+++ src/utils/useData.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+
+function useData(url) {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+      if (url) {
+        let ignore = false;
+        fetch(url)
+          .then((response) => response.json())
+          .then(json => {
+            if (!ignore) {
+              setData(json);
+            }
+          });
+        return () => {
+          ignore = true;
+        };
+      }
+    }, [url]);
+  
+    return data;
+  }
+
+  export {useData}
