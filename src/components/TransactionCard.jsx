43e0aacf9a5edffd559e891875ed0a0fbce4aec4
Dan <36828197+dangraham99@users.noreply.github.com>
2024-04-21 15:35:44 +0100

    removed dummy data; added dynamic display of fetched data

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -2,8 +2,9 @@ import { View, Text, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { styles } from '../assets/styles'
 import { useTheme } from '@react-navigation/native'
+import currency from 'currency.js'
 
-export default function TransactionCard() {
+export default function TransactionCard(props) {
 
 
     const { colors } = useTheme()
@@ -13,19 +14,19 @@ export default function TransactionCard() {
 
 
 
-    return (
+    return ( 
         <View style={[styles.transactionCard, { backgroundColor: colors.card }]}>
             <View>
                 <Image style={[styles.profileImg, { borderColor: colors.card }]} source={require(`../assets/placeholders/portrait-7.jpg`)} />
             </View>
             <View style={{ flex: 2, paddingLeft: 10 }}>
 
-                <Text style={[styles.cardTitle, { color: colors.text }]}>The Goldenhill</Text>
-                <Text style={{ color: colors.text }}>Daniel Graham</Text>
+                <Text style={[styles.cardTitle, { color: colors.text }]}>{props.transaction.title}</Text>
+                <Text style={{ color: colors.text }}>{ props.transaction.owner.name }</Text>
 
             </View>
             <View style={{ alignItems: 'flex-end', justifyContent: 'center', flex: 1 }}>
-                <Text style={[styles.cardTitle, { color: colors.text }]}>£4.20</Text>
+                <Text style={[styles.cardTitle, { color: colors.text }]}>{currency(props.transaction.valueInCents, {fromCents: true}).format({symbol: "£"})}</Text>
             </View>
         </View>
     )
